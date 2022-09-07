c67ac0a7498292087fc93edddd734a8e12d2bac0
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-09-07 21:27:45 +0100

    can fetcha random cat with full breed info - the game is on!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,37 @@
-
+import React, { useState, useEffect } from "react"
 import './App.css';
 
 function App() {
+  const [catId, setCatId] = useState([])
+  const [click, setClick] = useState(false)
+
+  useEffect(() => {
+    async function fetchBreeds() {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/breeds`,
+        { headers: { "x-api-key": "14445d3e-4465-488f-bc69-cd1a6c698a4c" } }
+      );
+      const data = await response.json();
+      setCatId(data.map((cat) => {
+        return cat.id
+      }));
+      console.log(catId);
+      setClick(true)
+      fetchCatById()
+      
+    }
+    async function fetchCatById() {
+      const breedId = catId[Math.floor(Math.random() * catId.length)]
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+        { headers: { "x-api-key": "14445d3e-4465-488f-bc69-cd1a6c698a4c" } }
+      );
+      const data = await response.json();
+      console.log(data)
+    }
+    fetchBreeds();
+  }, [click]);
+
   return (
     <div className="App">
      
