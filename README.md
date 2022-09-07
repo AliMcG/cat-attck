4732f9aa1c06d01d12db96712d4f692c5887e355
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2022-09-07 20:16:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cat Attack - When Cats Attack!
+
+A simple game to play with react.js and the Cat Api
---
+++ src/components/Display/Display.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Display() {
+  return ( <p>Display</p> );
+}
+
+export default Display;
